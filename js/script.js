8dad477c28a41c6f4cf771116ee2df46e916139e
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-11-15 00:23:05 +0400

    pushed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const phoneNumber = document.querySelector('.main__phone');
 const menuBtn = document.querySelector('.header__menu--btn');
 const menuBar = document.querySelector('.header__menu--mobile');
 const menuCloseBtn = document.querySelector('.close__menu-btn');
+const slider = document.querySelector('.production__slider');
 
 phoneNumber.addEventListener('click', () => {
     navigator.clipboard.writeText(phoneNumber.innerText);
@@ -15,3 +16,9 @@ menuBtn.addEventListener('click', () => {
 menuCloseBtn.addEventListener('click', () => {
     menuBar.classList.remove('activated');
 });
+
+slider.addEventListener('wheel', (e) => {
+    e.preventDefault();
+
+    slider.scrollLeft += e.deltaY;
+})
